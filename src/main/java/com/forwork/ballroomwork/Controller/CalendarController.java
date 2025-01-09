ef07554e5527b64f2bd2cea11eb8b2b8dfea4da8
olegberz <tipomodnij@gmail.com>
2025-01-09 15:21:55 +0200

    updated calendar

--- src/main/java/com/forwork/ballroomwork/Controller/CalendarController.java
+++ src/main/java/com/forwork/ballroomwork/Controller/CalendarController.java
@@ -3,6 +3,7 @@ package com.forwork.ballroomwork.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -12,10 +13,11 @@ import java.util.List;
 public class CalendarController {
 
     @GetMapping("/calendar")
-    public String getCalendar(Model model) {
+    public String getCalendar(@RequestParam(value = "year", required = false, defaultValue = "2025") int year,
+                              @RequestParam(value = "month", required = false, defaultValue = "0") int month,
+                              Model model) {
 
         Calendar calendar = Calendar.getInstance();
-
         calendar.set(Calendar.YEAR, 2025);
         calendar.set(Calendar.MONTH, Calendar.JANUARY);
 
@@ -34,9 +36,11 @@ public class CalendarController {
             calendarDays.add(String.valueOf(day));
         }
 
-        model.addAttribute("days", calendarDays);
-        model.addAttribute("month", "January");
-        model.addAttribute("year", "2025");
+        model.addAttribute("prevMonth", month == 0 ? 11 : month - 1);
+        model.addAttribute("nextMonth", month == 11 ? 0 : month + 1);
+        model.addAttribute("prevYear", month == 0 ? year - 1 : year);
+        model.addAttribute("nextYear", month == 11 ? year + 1 : year);
+
 
 
         return  "calendar";
